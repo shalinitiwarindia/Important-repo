4e58cad10bcf082423040125c5b5dd67af1a5754
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-04-26 21:25:00 +0530

    Update Find element occurrence in array.js

--- DSA/Find element occurrence in array.js
+++ DSA/Find element occurrence in array.js
@@ -1,1 +1,12 @@
+const arr = [1, 1, 2, 3, 1, 4];
+const count = {};
 
+for (const element of arr) {
+  if (count[element]) {
+    count[element] += 1;
+  } else {
+    count[element] = 1;
+  }
+}
+
+console.log(count); // 👉 {1: 3, 2: 1, 3: 1, 4: 1}
